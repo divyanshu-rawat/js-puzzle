deca04a9a4831fc6b297a9b5451e28784ef1f9b9
divyanshu <divyanshu.r46956@gmail.com>
2017-07-23 15:57:01 +0530

    done with Hint & Lint ! Project begins Now !

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,38 @@
 
 var gulp = require('gulp');
-var bs = require('browser-sync').create(); // creating a browser sync instance.
+
+// CREATING BROWSER-SYNC INSTANCE
+var bs = require('browser-sync').create(); 
+
+
+// JSCS & JS-HINT 
+
+var notify = require('gulp-notify');
+var growl = require('gulp-notify-growl');
+var jscs = require('gulp-jscs');
+var jshint = require('gulp-jshint');
+
+gulp.task('jscs', function() {
+    gulp.src(['./public/custom_javascript/*.js'])
+        .pipe(jscs())
+        .pipe(notify({
+            title: 'JSCS',
+            message: 'JSCS Passed. Congrats !'
+        }))
+});
+
+gulp.task('lint', function() {
+    gulp.src(['./public/custom_javascript/*.js'])
+        .pipe(jshint('.jshintrc'))
+        .pipe(jshint.reporter('jshint-stylish'))
+        .pipe(jshint.reporter('fail'))
+        .pipe(notify({
+            title: 'JSHint',
+            message: 'JSHint Passed. Let it fly!',
+        }))
+});
+
+
 
 gulp.task('browser-sync', function() {
     bs.init({
